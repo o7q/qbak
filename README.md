46298e53e0fa4b5c9389fa37098a1cd9bffdf79b
o7q <jev.o7q@gmail.com>
2022-09-02 19:57:43 -0500

    Minor update [v1.1.0]
    - Added a debug system
    - Fixed a major bug causing qbak to malfunction when a folder has a space in its name
    - Removed the max path character length
    - Enabled -O2 in the compiler (smaller executable size)
    - Small visual changes
    - Optimized code

--- README.md
+++ README.md
@@ -15,23 +15,39 @@
 <br>
 
 # Usage
-qbak is designed to work alongside a batch or console process. To use qbak, add the line `qbak.exe` inside of your batch script file. An example could be (minecraft server scenario):
+qbak is designed to work alongside a batch or console process. To use qbak, add the line `"qbak.exe"` inside of your batch script file. An example could be (minecraft server scenario):
 ```
 @echo off
 color a
-"jdk-18.0.2.1\bin\java.exe" -Xmx4G -Xms4G -jar "paper-1.19.2-135.jar"
-qbak.exe
-PAUSE
+:a
+cd "C:\MyCoolServer"
+"java_jdk\bin\java.exe" -Xmx4G -Xms4G -jar "server.jar"
+pause
+cd "C:"
+"qbak.exe"
+goto a
 ```
-In this example qbak would backup the specified directory after the server shuts down.
-
+In this example qbak would backup the specified directory after the server shuts down and a key is pressed.\
 qbak can also act as a normal desktop application as in executing it with a double-click.
 
 qbak will remember the directory you specified and it will continually use it unless changed. You can change the directory by running `qbak_reset.bat`, a file which is automatically created after launching qbak. Running `qbak_reset.bat` will cause qbak to ask you to specify a new directory on the next launch.
 
 qbak will organize the backups for you; all backups are housed within the `qbackups` folder, which will be automatically created after launching qbak. The folder structure of a backup will look like this: `qbackups\qbackup.8312022_15374\qbackup`.
 
-qbak will create a log for each backup it makes. The file path for the log will look like this: `qbackups\qbackup.8312022_15374\qbackup.log`.
+qbak will create a log for each backup it makes. The file path for the log will look like this: `qbackups\qbackup.8312022_15374\qbackup0.log`.
+
+<br>
+
+## <b>Debugging</b>
+I created some helpful commands for myself while debugging the program. I left them in because I think they could be helpful to you as well. To use a debug command, simply enter it after the `->` pointer.
+
+### <b>Commands</b>
+Inserting a `!` before a command tag will disable that command. For example: `$`<b>!</b>`DEBUG_PAUSE`
+
+`$DEBUG_PAUSE` Enables the pausing of the operation after it completes instead of closing\
+`$DEBUG_OUTCMD` Enables command outputs after the operation completes (works best when `$DEBUG_PAUSE` is enabled)\
+`$DEBUG_PURGE` Destroys all backups, this command is very dangorous and it will prompt you before executing\
+`$DEBUG_EXIT` Exits qbak properly
 
 <br>
 
--- src/qbak/main.cpp
+++ src/qbak/main.cpp
@@ -7,26 +7,39 @@
 
 using namespace std;
 
+void program();
+
+// configure functions
+void crtDir();
+void clr();
+
 // configure global variables
 string head = "$       __        __  \n"
               "$ ___ _/ /  ___ _/ /__\n"
               "$/ _ `/ _ \\/ _ `/  '_/\n"
               "$\\_, /_.__/\\_,_/_/\\_\\ \n"
               "$ /_/                 ";
-string ver = "v1.0.1";
+string ver = "v1.1.0";
 string cred = "                     by o7q";
+// program variables
+string bakNam;
+int logNum;
+bool debug_pause;
+bool debug_outcmd;
 
 main()
 {
      // configure time
      time_t n = time(NULL);
      tm *tm = localtime(&n);
+     // create time integers
      int m = 1 + tm->tm_mon;
      int d = tm->tm_mday;
      int y = 1900 + tm->tm_year;
      int t_h = tm->tm_hour;
      int t_m = tm->tm_min;
      int t_s = tm->tm_sec;
+     // ints -> strings
      string m_str = to_string(m);
      string d_str = to_string(d);
      string y_str = to_string(y);
@@ -34,11 +47,18 @@ main()
      string t_m_str = to_string(t_m);
      string t_s_str = to_string(t_s);
 
-     // create backups directory
-     string bakName = "qbackup." + m_str + d_str + y_str + "_" + t_h_str + t_m_str + t_s_str;
-     string dirCmd = "mkdir qbackups\\" + bakName + "\\qbackup";
-     system(dirCmd.c_str());
+     // create backup name and directory
+     bakNam = "qbackup." + m_str + d_str + y_str + "_" + t_h_str + t_m_str + t_s_str;
+     crtDir();
 
+     // start qbak
+     program();
+
+     return 1;
+}
+
+void program()
+{
      // create config reset
      string head_script = regex_replace(head, regex("\\$"), "echo ");
      ofstream crtRst;
@@ -60,22 +80,88 @@ main()
      string head_display = regex_replace(head, regex("\\$"), "");
      cout << "\n" +
                  head_display + ver + "\n" +
-                 cred + "\n\n";
+                 cred + "\n";
+     string n = debug_pause == true || debug_outcmd == true ? "\n" : "";
+     cout << n;
+     string pause0 = debug_pause == true ? "- DEBUG_PAUSE: ON\n" : "";
+     cout << pause0;
+     string outcmd0 = debug_outcmd == true ? "- DEBUG_OUTCMD: ON\n" : "";
+     cout << outcmd0;
+     cout << "\n";
 
      // check if config exists
-     string pthIn;
+     string in;
      ifstream chkCfg;
      chkCfg.open("qbackups\\qbak_cfg");
      if (!chkCfg)
      {
           // create config
-          cout << "Specify a directory:\n> ";
-          char pthIn[4096];
-          cin.getline(pthIn, sizeof(pthIn));
-          ofstream crtCfg;
-          crtCfg.open("qbackups\\qbak_cfg");
-          crtCfg << pthIn;
-          crtCfg.close();
+          cout << "Specify a directory or command:\n-> ";
+          getline(cin, in);
+          if (in != "$DEBUG_PAUSE" && in != "$!DEBUG_PAUSE" && in != "$DEBUG_OUTCMD" && in != "$!DEBUG_OUTCMD" && in != "$DEBUG_PURGE" && in != "$DEBUG_EXIT")
+          {
+               ofstream crtCfg;
+               crtCfg.open("qbackups\\qbak_cfg");
+               crtCfg << in;
+               crtCfg.close();
+          }
+     }
+
+     // debug_pause
+     if (in == "$DEBUG_PAUSE")
+     {
+          debug_pause = true;
+          clr();
+     }
+
+     // debug_!pause
+     if (in == "$!DEBUG_PAUSE")
+     {
+          debug_pause = false;
+          clr();
+     }
+
+     // debug_outcmd
+     if (in == "$DEBUG_OUTCMD")
+     {
+          debug_outcmd = true;
+          clr();
+     }
+
+     // debug_!outcmd
+     if (in == "$!DEBUG_OUTCMD")
+     {
+          debug_outcmd = false;
+          clr();
+     }
+
+     // debug_purge
+     if (in == "$DEBUG_PURGE")
+     {
+          cout << "\nAre you sure? (all backups will be destroyed!) [Y = Yes, N = No]\n-> ";
+          char prompt;
+          cin >> prompt;
+          if (prompt == 'Y' || prompt == 'y')
+          {
+               system("del \"qbak_reset.bat\" /f");
+               system("rmdir \"qbackups\" /s /q");
+               logNum = 0;
+               if (debug_outcmd == true)
+               {
+                    cout << "\nOUTCMD << del \"qbak_reset.bat\" /f\n";
+                    cout << "OUTCMD << \"qbackups\" /s /q\n";
+               }
+               crtDir();
+          }
+          cin.clear();
+          cin.sync();
+          clr();
+     }
+
+     // debug_exit
+     if (in == "$DEBUG_EXIT")
+     {
+          _Exit(0);
      }
 
      // read config
@@ -86,10 +172,49 @@ main()
 
      // format config
      cfg_str.erase(remove(cfg_str.begin(), cfg_str.end(), '"'), cfg_str.end());
-     cfg_str = "\"" + cfg_str + "\" ";
+     cfg_str = "'" + cfg_str + "' ";
 
      // backup directory work
-     string bakCmd = "powershell -command \"robocopy " + cfg_str + "\"qbackups\\" + bakName + "\\qbackup" + "\" " + "/e | tee-object " + "\"qbackups\\" + bakName + "\\qbackup.log\"";
+     string logNum_str = to_string(logNum);
+     string bakCmd = "powershell -command \"robocopy " + cfg_str + "'qbackups\\" + bakNam + "\\qbackup" + "' " + "/e | tee-object " + "\"qbackups\\" + bakNam + "\\qbackup" + logNum_str + ".log\"\"";
      system(bakCmd.c_str());
+     logNum += 1;
      cout << "\n\n";
+
+     // debug
+     string outcmd1 = debug_outcmd == true ? "\n\nOUTCMD << " + bakCmd : "";
+     cout << outcmd1;
+     if (debug_pause == true)
+     {
+          cout << "\n\n";
+          system("pause");
+          system("del \"qbackups\\qbak_cfg\" /f");
+          clr();
+     }
+
+     _Exit(0);
+}
+
+// create directory function
+void crtDir()
+{
+     string dirCmd = "mkdir qbackups\\" + bakNam + "\\qbackup";
+     system(dirCmd.c_str());
+
+     // debug
+     string outcmd = debug_outcmd == true ? "OUTCMD << mkdir qbackups\\" + bakNam + "\\qbackup" : "";
+     cout << outcmd;
+     string n = debug_outcmd == true ? "\n\n" : "\n";
+     if (debug_pause == true)
+     {
+          cout << n;
+          system("pause");
+     }
+}
+
+// clear function
+void clr()
+{
+     system("cls");
+     program();
 }
